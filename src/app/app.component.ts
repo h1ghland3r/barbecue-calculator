6c0efd96ddabd04ecd542e8f0fa8a305c89e8ffe
Railan Barbosa <railantb@gmail.com>
2024-05-05 21:16:50 +0100

    Data Binding, Pipes and Component Lifecycle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, LOCALE_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { registerLocaleData } from '@angular/common';
+import pt from '@angular/common/locales/pt';
+
+registerLocaleData(pt);
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, HeaderComponent],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'pt' },
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, Inject, inject } from '@angular/core';
+import { Component, Inject, LOCALE_ID, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { ScrollService } from '../../shared/services/scroll.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, MatButtonModule],
+  imports: [CommonModule, MatButtonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
@@ -20,7 +21,24 @@ export class HomeComponent {
 
   scrollService = inject(ScrollService);
 
+  welcomeMessage = 'Welcome to the Barbecue Calculator!';
+  name = 'Railan';
+
+  hasUser = true;
+
+  adaUrl = 'https://ada.tech';
+
+  displayButtonOutput = false;
+
   // constructor(private scrollService: ScrollService) { }
+
+  username = '';
+
+  hoje = new Date();
+
+  getName() {
+    return this.name;
+  }
 
   scrollToSection(id: string): void {
     this.scrollService.scrollToSection(id);
@@ -31,4 +49,10 @@ export class HomeComponent {
       this.sectionFourHidden = false;
     }
   }
+
+  onClick(): string {
+    this.displayButtonOutput = true;
+    return 'Você clicou no botão!';
+  }
+
 }
